1b445c9fe9c0bc1afc9e028fc5c10d5685be9f1c
kasunavodya@gmail.com <kasunavodya@gmail.com>
2022-10-23 23:57:04 +0530

    2022.10.23 - backend modified

--- Services/IOwnerService.cs
+++ Services/IOwnerService.cs
@@ -6,7 +6,6 @@ namespace FuelQ.Services
     {
         List<Owner> Get();
         Owner Get(string id);
-        Owner GetByEmail(string email);
         Owner Create(Owner owner);
         void Update(string id, Owner owner);
         void Remove(string id);
