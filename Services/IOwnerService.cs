c7acf1eba5a926d504a3a766d6e6e589047f43dd
kasunavodya <68581333+kasunavodya@users.noreply.github.com>
2022-10-24 18:26:26 +0530

    Update IOwnerService.cs

--- Services/IOwnerService.cs
+++ Services/IOwnerService.cs
@@ -6,6 +6,7 @@ namespace FuelQ.Services
     {
         List<Owner> Get();
         Owner Get(string id);
+        Owner GetByEmail(string email);
         Owner Create(Owner owner);
         void Update(string id, Owner owner);
         void Remove(string id);
